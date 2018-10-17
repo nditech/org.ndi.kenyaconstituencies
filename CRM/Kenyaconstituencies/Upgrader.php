bfde95d522ef0af56ace7e83627ed6a1289e014b
Mathieu Lutfy <mathieu@bidon.ca>
2018-10-17 11:27:20 -0400

    Regenerate civix boilerplates and upgrader code.

---
+++ CRM/Kenyaconstituencies/Upgrader.php
@@ -0,0 +1,26 @@
+<?php
+
+use CRM_Kenyaconstituencies_ExtensionUtil as E;
+
+/**
+ * Collection of upgrade steps.
+ */
+class CRM_Kenyaconstituencies_Upgrader extends CRM_Kenyaconstituencies_Upgrader_Base {
+
+  // By convention, functions that look like "function upgrade_NNNN()" are
+  // upgrade tasks. They are executed in order (like Drupal's hook_update_N).
+
+  /**
+   * Example: Run a couple simple queries.
+   *
+   * @return TRUE on success
+   * @throws Exception
+   *
+  public function upgrade_4200() {
+    $this->ctx->log->info('Applying update 4200');
+    CRM_Core_DAO::executeQuery('UPDATE foo SET bar = "whiz"');
+    CRM_Core_DAO::executeQuery('DELETE FROM bang WHERE willy = wonka(2)');
+    return TRUE;
+  } // */
+
+}
